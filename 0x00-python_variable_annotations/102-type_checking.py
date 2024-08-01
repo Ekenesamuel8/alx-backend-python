61871b727bef09a8ba372997a8aaf31dd351be91
ekenesamuel8 <chikwenduekene8@gmail.com>
2024-08-01 18:49:02 +0100

    any

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
